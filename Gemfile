590fe8b1738c9e1fa9fd0feebca1e0cb910cae7c
GeraldC <info@cerebral-engine.com>
2011-02-10 00:45:17 -0800

    third commit

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '3.0.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
+gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
   # GET /microposts
   # GET /microposts.xml
   def index
-    @microposts = Micropost.all
+    @microposts = Micropost.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
